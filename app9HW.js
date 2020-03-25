85715c55766cab69880de09194724d9a2332566e
Vladimir <chuwoza@gmail.com>
2020-03-25 16:42:38 +0200

    add fix

--- app9HW.js
+++ app9HW.js
@@ -16,16 +16,29 @@
 // 	alert(`Уважаемый ${a} , за 6 месяцев вы получите: ${d.toFixed(2) } грн. дохода от депозита!
 // Общая сумма доступная к снятию: ${b + d} грн.`);
 
-let season = prompt("Введите пору року", "");
+// Определить время года
 
-if (season === "зима"){
-	alert ("Зараз зима")	
-} else if (season === "весна") {
-	alert ("Зараз весна")
-} else if (season === "літо") {
-	alert ("Зараз літо")
-} else if (season === "oсінь"){
-	alert ("Зараз oсінь")
-} else {
-	alert("Такої пори року як " + season + " не існує")
-}
+// let season = prompt("Введите пору року", "");
+
+// if (season === "зима"){
+// 	alert ("Зараз зима")	
+// } else if (season === "весна") {
+// 	alert ("Зараз весна")
+// } else if (season === "літо") {
+// 	alert ("Зараз літо")
+// } else if (season === "oсінь"){
+// 	alert ("Зараз oсінь")
+// } else {
+// 	alert("Такої пори року як " + season + " не існує")
+// }
+
+// Стоимость товара со скидкой:
+
+
+// let discount = 20;
+// let currency = "грн.";
+// let productPrice = +prompt("Введите вартість товару", "");
+// let productPriceWithDiscount = productPrice - (productPrice*discount/100);
+
+// console.log("Стоимость товара со скидкой" + " " + productPriceWithDiscount + " "+"грн.");
+
