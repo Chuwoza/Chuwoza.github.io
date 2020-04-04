32abc47fc13f9ed0492e098e434308fdc4bc395f
Vladimir <chuwoza313@gmail.com>
2020-04-04 21:56:07 +0300

    add app.js with 1st code cart

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+let productsCountEl = document.getElementById("products-count");
+console.log(productsCountEl);
+
+let addToCartButtons = document.querySelectorAll(".btn-add-to-cart");
+console.log(addToCartButtons);
+
+for( let i = 0 ; i < addToCartButtons.length; i++ ) {
+	addToCartButtons[i].addEventListener("click", function() {
+		let prevProductCount = +productsCountEl.textContent;
+		productsCountEl.textContent = prevProductCount + 1;
+	})
+}
