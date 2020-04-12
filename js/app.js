d6b41a535ba6fb371bb63b76f9367547d8c51d64
Vladimir <chuwoza@gmail.com>
2020-04-12 17:08:14 +0300

    add slick

--- js/app.js
+++ js/app.js
@@ -73,11 +73,11 @@ function curBtn(numBtn){
 
 			//add slick slider
 
-// $('.slider-block').slick({
-//     autoplay:true,
-//     autoplaySpeed:2000,
-//     dots:true,
-// })
+$('.slider-block').slick({
+    autoplay:true,
+    autoplaySpeed:2000,
+    dots:true,
+})
 
 // change color
 
