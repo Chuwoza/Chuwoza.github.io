7a554d9c793db153fbde381911f84a51c02ced6f
Vladimir <chuwoza@gmail.com>
2020-04-13 14:10:49 +0300

    add slider-foto

--- js/app.js
+++ js/app.js
@@ -16,60 +16,102 @@
 
 // Второй вариант - правильный
 
-let productsCountEl = document.getElementById("products-count");
-console.log(productsCountEl);
+// let productsCountEl = document.getElementById("products-count");
+// console.log(productsCountEl);
 
-let addToCartButtons = document.querySelectorAll(".btn-add-to-cart");
-console.log(addToCartButtons);
+// let addToCartButtons = document.querySelectorAll(".btn-add-to-cart");
+// console.log(addToCartButtons);
 
-for( let i = 0 ; i < addToCartButtons.length; i++ ) {
-	addToCartButtons[i].addEventListener("click", function() {
-		productsCountEl.textContent = +productsCountEl.textContent +1;
-	})
-}
+// for( let i = 0 ; i < addToCartButtons.length; i++ ) {
+// 	addToCartButtons[i].addEventListener("click", function() {
+// 		productsCountEl.textContent = +productsCountEl.textContent +1;
+// 	})
+// }
 
 			//change product quantity - изменить кол-во продукта
 
-let decrementBtn = document.querySelector('.btn-decrement');
-let incrementBtn = document.querySelector('.btn-increment');
-let quantityInput = document.querySelector(".product-quantity input");
-console.log(decrementBtn);
+// let decrementBtn = document.querySelectorAll('.btn-decrement');
+// let incrementBtn = document.querySelectorAll('.btn-increment');
+// let quantityInput = document.querySelectorAll(".product-quantity input");
+// console.log(decrementBtn);
 // ==== проверить правильно ли назначили переменные! ====
 //console.log(decrementBtn);
 //console.log(incrementBtn);
 //console.log(quantityInput);
 
 //чтобы кол-во товара не могло быть ноль при первой загрузке
-function startData (){
-	let currentCount = +quantityInput.value;
-	if(currentCount <= 1) {
-		decrementBtn.disabled = true;
-	}else {
-		decrementBtn.disabled = false;
-}};
+// function startData (){
+// 	let currentCount = +quantityInput.value;
+// 	if(currentCount <= 1) {
+// 		decrementBtn.disabled = true;
+// 	}else {
+// 		decrementBtn.disabled = false;
+// }};
 
-startData();
+// startData();
 
-function curBtn(numBtn){
-	let currentCount = +quantityInput.value;
-	let nextCount = currentCount + numBtn;
+// function curBtn(numBtn){
+// 	let currentCount = +quantityInput.value;
+// 	let nextCount = currentCount + numBtn;
 
-	quantityInput.value = nextCount;
-	if (nextCount <= 1){
-		decrementBtn.disabled = true;
-	// }else if(nextCount >= 5){
-	// incrementBtn.disabled = true;
-	}else {
-		decrementBtn.disabled = false;
+// 	quantityInput.value = nextCount;
+// 	if (nextCount <= 1){
+// 		decrementBtn.disabled = true;
+// 	// }else if(nextCount >= 5){
+// 	// incrementBtn.disabled = true;
+// 	}else {
+// 		decrementBtn.disabled = false;
+// 	}
+// }
+// // ES6. Стрелочные функции. This - больше не проблема!!!
+
+// // for (i = 0 ; i < decrementBtn.length ; i++) {
+// 	incrementBtn.addEventListener("click", () => curBtn(1))
+// // };
+
+// 	decrementBtn.addEventListener("click", () => curBtn(-1));
+
+let products = document.querySelectorAll(".good-info");
+let productsCountEl = document.getElementById("products-count");
+
+// function startData (){
+// 	let currentCount = +quantityInput.value;
+// 	if(currentCount <= 1) {
+// 		decrementBtn.disabled = true;
+// 	}else {
+	
+function applyValue(btn, oppositeBtn, input, value){
+	let nextCount = (+input.value) + value;
+
+
+btn.disabled = false;
+oppositeBtn.disabled = false;
+input.value = nextCount;
+
+	if (nextCount <= 1 || nextCount > 4){
+		btn.disabled = true;
+        return;
 	}
+};
+
+for(var i = 0; i<products.length;i++){
+    let decrementBtn = products[i].querySelector('.btn-decrement');
+    let incrementBtn = products[i].querySelector('.btn-increment');
+    let quantityInput = products[i].querySelector("input");
+    let addToCartBtn = products[i].querySelector(".btn-add-to-cart");
+
+console.log(addToCartBtn);
+    addToCartBtn.addEventListener("click", function() {
+		productsCountEl.textContent = +productsCountEl.textContent + +quantityInput.value;
+quantityInput.value = 1;
+	});            
+
+   incrementBtn.addEventListener("click", function(e){ applyValue(incrementBtn, decrementBtn, quantityInput, 1);});
+
+	decrementBtn.addEventListener("click", () => applyValue(decrementBtn, incrementBtn, quantityInput, -1));
+
 }
-// ES6. Стрелочные функции. This - больше не проблема!!!
 
-// for (i = 0 ; i < decrementBtn.length ; i++) {
-	incrementBtn.addEventListener("click", () => curBtn(1))
-// };
-
-	decrementBtn.addEventListener("click", () => curBtn(-1));
 
 			//add slick slider
 
